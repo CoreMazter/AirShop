e80469fe301d0475d72d285f5d7cab598b1b1be2
ChelohHolmes <marcelogerstein@gmail.com>
2019-05-14 15:07:07 -0500

    Fix user service

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  URL ='/adquisicion';
-  info : any;
+  URL = '/adquisicion';
+  info: any;
 
   constructor(private http: HttpClient) { }
 
-  getContenido() {
-  return this.info = this.http.get(environment.serverUrl+this.URL);
+  getContenido(token) {
+  return this.info = this.http.get('http://192.168.84.123:6543' + this.URL + '?token=' + token.token);
   }
 }
