faf56da180b6798734922a37f0ab759127765a14
ChelohHolmes <marcelogerstein@gmail.com>
2019-05-14 15:06:54 -0500

    Fix profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,30 +8,15 @@ import { UserService } from '../Services/user.service';
 })
 export class ProfileComponent implements OnInit {
 
-  flights:any;
-  constructor(private info:UserService) { }
+  flights: any;
+  constructor(private info: UserService) { }
 
   ngOnInit() {
-    
-     this.info.getContenido().subscribe(content=>{this.flights=content;console.log(this.flights);});
+
+     this.info.getContenido(JSON.parse(localStorage.getItem('token'))).subscribe(content => {
+       this.flights = content; console.log(this.flights); });
 
   }
-
-  // flights:any[]=[
-  //   {
-  //     vuelo:"AA123",
-  //     aerolinea:"Volaris",
-  //     origen:"Guadalajara",
-  //     destino:"CDMX"
-  //   },
-  //   {
-  //     vuelo:"AA345",
-  //     aerolinea:"Viva Aerobús",
-  //     origen:"CDMX",
-  //     destino:"Guanajuato"
-  //   },
-    
-  // ];
 }
 
 
