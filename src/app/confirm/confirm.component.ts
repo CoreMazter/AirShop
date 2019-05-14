3227361647a4fadecc78060b5161496e824f01bc
ChelohHolmes <marcelogerstein@gmail.com>
2019-05-14 15:04:35 -0500

    Fix confirm component

--- src/app/confirm/confirm.component.ts
+++ src/app/confirm/confirm.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ConfirmService} from '../Services/confirm.service';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-confirm',
@@ -7,8 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmComponent implements OnInit {
 
-  constructor() {}
+  constructor(private http: ConfirmService, private snackBar: MatSnackBar) {}
 
+  url = '/adquisicion';
   order: any[] = [];
 
   ngOnInit() {
@@ -18,4 +21,15 @@ export class ConfirmComponent implements OnInit {
   }
 
 
+  onSubmit() {
+    this.http.url = this.url;
+    console.log(this.order);
+    for (let i = 0; i < this.order.length; i++) {
+      console.log(this.order[i]);
+      this.http.postMethod(this.order[i]).subscribe(hola => {
+        this.snackBar.open('Boleto comprado', 'Ok', {duration: 5000 });
+      }, error => this.snackBar.open('Error. No se pudo realizar la compra', 'NotOk', {duration: 5000}));
+    }
+    localStorage.removeItem('vuelo');
+  }
 }
